d9aa6a0e9027cb2c17b9b8a9c070025ce221ba64
Alexey <alekseynovikov65@gmail.com>
2023-01-15 22:50:02 +0300

    Initialize input arrays

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿string[] inputArray1 = new string[]
+{
+    "Hello", "2", "world", ":-)"
+};
+string[] inputArray2 = new string[]
+{
+    "1234", "1567", "-2", "computer science"
+};
+string[] inputArray3 = new string[]
+{
+    "Russia", "Denmark", "Kazan"
+};
